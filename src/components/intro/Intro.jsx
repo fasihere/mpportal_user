5b897e6dc7bac060d40f1886889d0d3e6e5f7ccb
Rishabh Upadhyay <87112835+rishabh-na20b052@users.noreply.github.com>
2021-08-17 15:28:36 +0530

    Add files via upload

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom'
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            <div className="left">
-                <h1>Write to your MP,<br /><span>Dean Kuriakose</span></h1>
-                <Link className="btn link" to="/request/new">Write Now</Link>
+            <div className="quote">
+                   "Be the change that you wish to see in the world."<h5>~ MAHATMA GANDHI</h5>
             </div>
-            <div className="right">
-                <img src="public/assets/images/dean_img.jpeg"  width="100px" alt="" />
+            <div className="bottom">           
+                <img src="/assets/images/intro-dp.png" alt="dp"/>        
+                <div  className="left">
+                    <h1>Write to your MP,<br /><span>Dean Kuriakose</span></h1>
+                    <Link className="btn link" to="/request/new">Write Now</Link>
+                </div> 
             </div>
         </div>
     )
