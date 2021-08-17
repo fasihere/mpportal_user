ba11379a1421461529282faf5338be56dacf047f
THARUN.V <85789403+VTHARUN01@users.noreply.github.com>
2021-08-17 18:21:55 +0530

    Add files via upload

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,29 +1,47 @@
 import './about.scss'
 
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
 export default function About() {
     return (
-        <div className="about" id="about">
-            <div className="wrapper">
-                <div className="left">
-                    <h1>About</h1>
-                    <div className="content">
-                        <p>An academic, advocate and a politician, Dean Kuriakose holds variety of experiences and positions.</p><br />
-                        <p>Dean was born on 27 June 1981, to Adv. A. M Kuriakose and Rosamma from Kulappuram, Paingottoor.</p><br /><br />
-                        <p>
-                            Dean underwent schoolings at his hometown and within Idukki district. Dean began his pre-graduate 
-                            studies at New Man college Thodupuzha, followed by graduation in Bsc.Physics in St.Joseph's college 
-                            Molamattom before pursuing his LLB from Kerala Law Academy Law College, Trivandrum. Dean graduated 
-                            LLB and enrolled as an advocate on 2005. 
-                        </p><br /><br />
-                        <p>
-                            Dean was awarded with 1st rank by Mahatma Gandhi university for M.A in Human Rights & Politics in 2014
-                            and is currently pursuing Ph.D in International Relations and Politics from Central University of Kerala.
-                        </p><br /><br />
-                        <p>Dean, supported by his wife, Dr. Neetha Paul and their baby boy, currently resides in Thodupuzha and New Delhi, India.</p>
+        <div id="about" className="start">
+            <h1 className="h1">ABOUT</h1>
+            <h6></h6>
+            <Row>
+                <Col xs={{ span: 12 }}><img src="/assets/images/dean_img.png" /></Col>
+            </Row>
+            <Row className="center">
+                <Col className="sec" xs={{ span: 11 }} md={{ span: 4, offset: 0 }} >
+                    <div className="icon"> <i class="fas fa-globe-asia"></i></div>
+                    <div className="heading">LIFE</div>
+                    <h6></h6>
+                    <div>Dean was born on 27 June 1981, to Adv. A. M Kuriakose and Rosamma from Kulappuram, Paingottoor.
+                        .Dean, supported by his wife, Dr. Neetha Paul and their baby boy, currently resides in Thodupuzha and New Delhi, India.
                     </div>
-                </div>
-                <div className="right"></div>
-            </div>
-        </div>
+                </Col>
+                <Col className="sec" xs={{ span: 11 }} md={{ span: 4, offset: 0 }} >
+                    <div className="icon"><i class="fas fa-user-graduate"></i></div>
+                    <div className="heading">STUDIES</div>
+                    <h6></h6>
+                    <div> Dean underwent schoolings at his hometown and within Idukki district. Dean began his pre-graduate
+                        studies at New Man college Thodupuzha, followed bygraduation in Bsc.Physics in St.Joseph's college
+                        Molamattom before pursuing his LLB from Kerala Law Academy Law College, Trivandrum. Dean graduated
+                        LLB and enrolled as an advocate on 2005 </div></Col>
+                <Col className="sec" xs={{ span: 11 }} md={{ span: 4, offset: 0 }}>
+                    <div className="icon"><i className="fas fa-trophy"  ></i></div>
+                    <div className="heading">AWARDS</div>
+                    <h6></h6>
+                    <div>
+                        An academic, advocate and a politician, Dean Kuriakose holds variety of experiences and positions.
+                        Dean was awarded with 1st rank by Mahatma Gandhi university for M.A in Human Rights & Politics in 2014
+                        and is currently pursuing Ph.D in International Relations and Politics from Central University of Kerala.
+                    </div>
+                </Col>
+            </Row>
+
+        </div >
+
     )
 }
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,71 @@
 import './contact.scss'
 import { Person, Mail } from '@material-ui/icons'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Contact() {
     return (
-        <div className="contact" id="contact">
-            <div className="wrapper">
-                <div className="left">
-                    <h2>Offices of Dean Kuriakose</h2>
-                    <div className="content">
-                        <p>90, South Avenue, South Avenue Area, President's Estate, New Delhi, Delhi 110011, India</p>
-                        <p>Opp. PWD Guest House, Telephone Exchange Road, Thodupuzha P.O, Idukki district, Kerala, India - 685 584 </p>
-                        <p>Aalikunnel Building, Idukki Colony P.O, Cheruthoni, Idukki district, Kerala, India - 685 602</p>
+        <div id="contact" className="start1">
+            <h1 className="h1">CONTACT US</h1>
+            <h6></h6>
+            <Row className="top">
+
+                <Col className="sec1" xs={{ span: 11 }} md={{ span: 4, offset: 1 }} >
+                    <div className="icon"> <i class="fas fa-briefcase"></i></div>
+                    <div className="heading">ADDRESS</div>
+                    <h6></h6>
+                    <ul>
+                        <li>90, South Avenue, South Avenue Area, President's Estate, New Delhi, Delhi 110011, India
+                        </li>
+                        <li>Opp. PWD Guest House, Telephone Exchange Road, Thodupuzha P.O, Idukki district, Kerala, India - 685 584 </li>
+                        <li>
+                            Aalikunnel Building, Idukki Colony P.O, Cheruthoni, Idukki district, Kerala, India - 685 602
+                        </li>
+                    </ul>
+                </Col>
+                <Col className="map" md={{ span: 5, offset: 1 }}>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d3502.7920246653016!2d77.19684701508199!3d28.6060153824283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x390ce2a6930989ab%3A0x6d62871fcf67dc58!2s90%2C%20South%20Ave%2C%20South%20Avenue%20Area%2C%20President&#39;s%20Estate%2C%20New%20Delhi%2C%20Delhi%20110011!3m2!1d28.6060154!2d77.1990357!5e0!3m2!1sen!2sin!4v1626114835194!5m2!1sen!2sin" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" title="map"></iframe>
+                </Col>
+
+
+
+            </Row>
+
+            <Row className="center">
+
+                <Col xs={{ span: 12 }} md={{ span: 4, offset: 1 }}>
+                    <h3 className="h3">GET IN TOUCH</h3>
+                    <h6></h6>
+                    <ul className="con">
+                        <li className="coo"><Person className="icon" ></Person>
+                            <span className="colo">+91 94478 77369</span>
+                        </li>
+                        <li className="coo">
+                            <Mail className="icon" />
+                            <a className="colo" href="https://mail.google.com/mail/u/0/#inbox">deankuriakosemp@gmail.com</a>
+                        </li>
+                        <li className="coo left1">
+                            <a className="coo" href="https://www.instagram.com/" > <i class="fab fa-instagram"></i></a></li>
+
+                        <li className="coo left1">
+                            <a className="coo" href="https://www.facebook.com/"> <i class="fab fa-facebook"></i></a></li>
+                        <li className="coo left1">
+                            <a className="coo" href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a></li>
+                        <li className="coo left1"><a className="coo" href="https://www.twitter.com/"><i class="fab fa-twitter-square"></i></a></li>
+                    </ul>
+                </Col>
+                <Col xs={{ order: "first" }} md={{ span: 4, offset: 1 }} >
+                    <div className="mail" >
+                        <img src="assets/images/undraw_contact_us_15o2.png" />
+
                     </div>
-                    <div className="itemContainer">
-                        <Person className="icon"/>
-                        <span>+91 94478 77369</span>
-                    </div>
-                    <div className="itemContainer">
-                        <Mail className="icon"/>
-                        <span><a href="deankuriakosemp@gmail.com">deankuriakosemp@gmail.com</a></span>
-                    </div>
-                </div>
-                <div className="right">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d3502.7920246653016!2d77.19684701508199!3d28.6060153824283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x390ce2a6930989ab%3A0x6d62871fcf67dc58!2s90%2C%20South%20Ave%2C%20South%20Avenue%20Area%2C%20President&#39;s%20Estate%2C%20New%20Delhi%2C%20Delhi%20110011!3m2!1d28.6060154!2d77.1990357!5e0!3m2!1sen!2sin!4v1626114835194!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" title="map"></iframe>
-                </div>
-            </div>
-        </div>
+
+                </Col>
+
+            </Row>
+
+
+        </div >
+
     )
 }
