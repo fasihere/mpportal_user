4ffed310fa1e4863fe786fd6c73f4439a860604b
THARUN.V <85789403+VTHARUN01@users.noreply.github.com>
2021-08-20 09:55:36 +0530

    Add files via upload

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,6 @@ import './contact.scss'
 import { Person, Mail } from '@material-ui/icons'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
 export default function Contact() {
     return (
         <div id="contact" className="start1">
@@ -23,14 +22,10 @@ export default function Contact() {
                         </li>
                     </ul>
                 </Col>
-                <Col data-aos="fade-up" data-aos-duration="1000" className="map" md={{ span: 5, offset: 1 }}>
+                <Col className="map" md={{ span: 5, offset: 1 }}>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d3502.7920246653016!2d77.19684701508199!3d28.6060153824283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x390ce2a6930989ab%3A0x6d62871fcf67dc58!2s90%2C%20South%20Ave%2C%20South%20Avenue%20Area%2C%20President&#39;s%20Estate%2C%20New%20Delhi%2C%20Delhi%20110011!3m2!1d28.6060154!2d77.1990357!5e0!3m2!1sen!2sin!4v1626114835194!5m2!1sen!2sin" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" title="map"></iframe>
                 </Col>
-
-
-
             </Row>
-
             <Row >
 
                 <Col xs={{ span: 12 }} md={{ span: 4, offset: 1 }}>
